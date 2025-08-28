3a5bf14ae3116a1ceb209711db25391313f97e38
3dgopnik <45687738+3dgopnik@users.noreply.github.com>
2025-08-28 17:01:05 +0300

    Update controller.py
    ф

--- ui/controller.py
+++ ui/controller.py
@@ -1,1 +1,10 @@
+# ui/revoice/controller.py
+class Controller:
+    def __init__(self):
+        """Initialize the controller"""
+        pass
+
+    def run(self):
+        """Execute controller logic"""
+        pass
 
